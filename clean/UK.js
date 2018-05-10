387dd3f53c59ca926dade61017d4367f4fb056bc
Chris Zubak-Skees <chriszs@gmail.com>
2018-05-10 18:18:49 -0400

    Also split by new line

--- clean/UK.js
+++ clean/UK.js
@@ -6,9 +6,12 @@ let data = fs.readFileSync(__dirname + '/../data/UK.csv','utf8')
 let rows = dsv.csvParseRows(data);
 
 for (let row of rows) {
-    let parts = row[0].split(/\W{2,}/g);
+    let parts = row[0].split(/(\W{2,}|\n)/g);
     // if (parts)
     if (parts[0].match(/^[A-Z ]+$/g)) {
-        console.log(parts[0]);
+        console.log('== ' + parts[0]);
+    }
+    else {
+    	console.log(row);
     }
 }
