19364a59ecef40aa1ad55422b70c369afe087ab3
Chris Zubak-Skees <chriszs@gmail.com>
2018-05-11 12:37:54 -0400

    More UK clean up code

--- clean/UK.js
+++ clean/UK.js
@@ -1,17 +1,50 @@
 let dsv = require('d3-dsv');
 let fs = require('fs');
 
-let data = fs.readFileSync(__dirname + '/../data/UK.csv','utf8')
+let data = fs.readFileSync(__dirname + '/../data/UK.csv', 'utf8');
 
 let rows = dsv.csvParseRows(data);
 
+let fields = {};
+
 for (let row of rows) {
-    let parts = row[0].split(/(\W{2,}|\n)/g);
-    // if (parts)
-    if (parts[0].match(/^[A-Z ]+$/g)) {
+    // console.log(row.join('  '));
+
+    let parts = row[0].split(/( {2,}|\n|\r)/g);
+
+    let rowValues = [];
+
+    // country header
+    if (parts[0].match(/^[A-Z &]+$/g)) {
+        console.log(fields);
+
+        fields = {};
         console.log('== ' + parts[0]);
+        rowValues = parts.slice(1).concat(row.slice(1));
+    } else {
+        rowValues = row;
     }
-    else {
-    	console.log(row);
+
+    let fieldValue = '';
+    for (i in rowValues) {
+        let fieldName = '';
+
+        // field name
+        if (rowValues[i].includes(':') || rowValues[i].includes('Currency')) {
+            if (rowValues[i].replace(':', '').trim() !== '') {
+                fieldName = rowValues[i].replace(':', '').trim();
+
+                console.log(fieldName);
+
+                if (fieldValue.trim() !== '') {
+                    fields[fieldName.trim()] = fieldValue.trim();
+                }
+
+                fieldValue = '';
+            }
+        } else {
+            // field value
+            fieldValue += rowValues[i];
+        }
     }
 }
