2689add9ed08d6daeef85c8301a1c6d229a64d63
Chris Zubak-Skees <chriszs@gmail.com>
2018-05-15 18:57:03 -0400

    Clean UN, Canada

---
+++ clean/Canada.js
@@ -0,0 +1,44 @@
+let dsv = require('d3-dsv');
+let fs = require('fs');
+
+let data = fs.readFileSync(__dirname + '/../data/Canada.csv', 'utf8');
+
+let rows = dsv.csvParseRows(data);
+
+let country = '';
+let currency = '';
+
+let headers = [
+    'Country',
+    'Currency',
+    'Location',
+    'Breakfast',
+    'Lunch',
+    'Dinner',
+    'Total Meals',
+    'Incidental Amount',
+    'GRAND TOTAL (taxes included)'
+];
+
+rows = rows
+    .map(row => {
+        if (row[1] === '') {
+            country = row[0].split(/ [––] Currency: /)[0];
+            currency = row[0].split(/ [––] Currency: /)[1];
+        } else {
+            row.unshift(currency);
+            row.unshift(country);
+        }
+
+        return row;
+    })
+    .filter(row => row[1] !== '')
+    .slice(3);
+
+rows.unshift(headers);
+
+fs.writeFileSync(
+    __dirname + '/../cleaned/Canada.csv',
+    dsv.csvFormatRows(rows),
+    'utf8'
+);
---
+++ clean/UN.js
@@ -0,0 +1,35 @@
+let dsv = require('d3-dsv');
+let fs = require('fs');
+
+let data = fs.readFileSync(__dirname + '/../data/UN.csv', 'utf8');
+
+let rows = dsv.csvParseRows(data);
+
+let country = '';
+let currency = '';
+
+rows = rows
+    .map(row => {
+        if (row[0] == 'Country - (Currency) Area') {
+            row.shift();
+            row.unshift('Location');
+            row.unshift('Currency');
+            row.unshift('Country');
+        }
+        else if (row[1] === '') {
+            country = row[0].split(' (')[0];
+            currency = row[0].split(' (')[1].replace(')', '');
+        } else {
+            row.unshift(currency);
+            row.unshift(country);
+        }
+
+        return row;
+    })
+    .filter(row => row[1] !== '');
+
+fs.writeFileSync(
+    __dirname + '/../cleaned/UN.csv',
+    dsv.csvFormatRows(rows),
+    'utf8'
+);
