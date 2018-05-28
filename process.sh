c1610d23629ba446c86c7f309227c774c0ec4331
Chris Zubak-Skees <chriszs@gmail.com>
2018-05-28 18:50:15 -0400

    Change output CSV name to results.csv

--- process.sh
+++ process.sh
@@ -6,4 +6,4 @@ node ./clean/UN
 echo "Country,Location,Season Code,Season Start Date,Season End Date,Lodging,Meals & Incidentals,Per Diem,Effective Date ,Footnote Reference,Location Code" > ./cleaned/US.csv
 tail -n +2 ./data/US.csv >> ./cleaned/US.csv
 cp ./data/EU.csv ./cleaned/EU.csv
-node collect > out.csv
+node collect > results.csv
