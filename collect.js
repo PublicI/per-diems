9ee4c9631fabbf10445508187f8789c16f941046
Chris Zubak-Skees <chriszs@gmail.com>
2018-06-02 12:15:03 -0400

    Refactor to use functions, seems to fix a bug with UK currency conversion

--- collect.js
+++ collect.js
@@ -18,9 +18,123 @@ money.rates = currency.rates;
 // set up map we'll keep the locations in
 let locations = {};
 
-// read all the files in /cleaned/ in one by one
-// each one corresponds to a jurisdiction like Canada, etc. that sets per diems
-files.map(file => {
+function extractMoneyValue(value) {
+    if (!value) {
+        return NaN;
+    }
+    let result = parseFloat(value.replace(/[^0-9.]*/g, ''));
+    return result;
+}
+
+function extractCurrencyCode(value) {
+    if (!value) {
+        return null;
+    }
+    let matches = value.match(/\(*[A-Z]{3}\)*/);
+    let code = matches ? matches[0].replace('(', '').replace(')', '') : null;
+    return code in currency.rates ? code : null;
+}
+
+function cleanMoney(code, value) {
+    if (typeof value === 'undefined' || value == '*') {
+        return NaN;
+    }
+
+    let currencyCode = extractCurrencyCode(code);
+    let moneyValue = extractMoneyValue(value);
+
+    if (!isNaN(moneyValue)) {
+        if (currencyCode) {
+            return money.convert(moneyValue, {
+                from: currencyCode,
+                to: 'USD'
+            });
+        } else {
+            return moneyValue;
+        }
+    } else {
+        return NaN;
+    }
+}
+
+function processLocation(country, location) {}
+
+function processRow(locations, file, row) {
+    // set up empty string for location, if there isn't one we'll use this instead
+    // each row has a field named country and one called location
+    // we need to use these to match per diem rates, but they're messy
+    // so we're going to clean them up
+    let location = '';
+
+    // remove catch-alls like other, elsewhere, all areas and the name of the country
+    // remove parentheticals
+    if (row.Location) {
+        location =
+            row.Location.includes('Other') ||
+            row.Location.includes('Elsewhere') ||
+            row.Location.includes('All Areas') ||
+            row.Location.toUpperCase().includes(row.Country.toUpperCase())
+                ? ''
+                : '-' + row.Location.split(' (')[0];
+    }
+
+    // convert the country and location to a lowercase slug like afghanistan-kabul
+    let slug = slugify(row.Country + location, {
+        replacement: '-',
+        lower: true
+    });
+
+    // if the slug isn't in the locations map yet, add an empty object for it
+    if (!(slug in locations)) {
+        locations[slug] = {};
+    }
+
+    // find the field that corresponds to the per diem for this particular jurisidiction
+    let moneyTotal = [
+        'First 60 Days US$',
+        'Per Diem',
+        'Total residual',
+        'GRAND TOTAL (taxes included)',
+        'Amount (Euros)'
+    ]
+        .map(key => row[key])
+        .find(value => value);
+
+    let currencyCode = row.Currency;
+
+    // but if the column header says this should be in dollars, respect that
+    if (row['First 60 Days US$']) {
+        currencyCode = null;
+    }
+    // if the column header says this should be in Euros, respect that
+    if (row['Amount (Euros)']) {
+        currencyCode = 'EUR';
+    }
+
+    moneyTotal = cleanMoney(currencyCode, moneyTotal);
+
+    // if we're in the UK, add the room rate after possible conversion
+    let roomRate = processMoney(
+        row['Room rate'] + ' ' + row.Currency,
+        row['Room rate']
+    );
+    moneyTotal += !isNaN(roomRate) ? roomRate : 0;
+
+    // store the rate in its corresponding map location
+    if (!isNaN(moneyTotal)) {
+        locations[slug][file.replace('.csv', '')] = moneyTotal;
+    }
+
+    // include US meals & incidentals as a separate column for comparison to Canada
+    if ('Meals & Incidentals' in row) {
+        locations[slug]['US meals & incidentals'] = processMoney(
+            null,
+            row['Meals & Incidentals']
+        );
+    }
+}
+
+function processFile(locations, file) {
     let data = fs.readFileSync(path + file, 'utf8');
 
     // strip UTF8 byte order mark, see https://github.com/nodejs/node-v0.x-archive/issues/1918
@@ -30,117 +144,12 @@ files.map(file => {
     let rows = dsv.csvParse(data);
 
     // for each row, representing a per diem rate...
-    rows.forEach(row => {
-        // set up empty string for location, if there isn't one we'll use this instead
-        // each row has a field named country and one called location
-        // we need to use these to match per diem rates, but they're messy
-        // so we're going to clean them up
-        let location = '';
+    rows.forEach(processRow.bind(this, locations, file));
+}
 
-        // remove catch-alls like other, elsewhere, all areas and the name of the country
-        // remove parentheticals
-        if (row.Location) {
-            location =
-                row.Location.includes('Other') ||
-                row.Location.includes('Elsewhere') ||
-                row.Location.includes('All Areas') ||
-                row.Location.toUpperCase().includes(row.Country.toUpperCase())
-                    ? ''
-                    : '-' + row.Location.split(' (')[0];
-        }
-
-        // convert the country and location to a lowercase slug like afghanistan-kabul
-        let slug = slugify(row.Country + location, {
-            replacement: '-',
-            lower: true
-        });
-
-        // if the slug isn't in the locations map yet, add an empty object for it 
-        if (!(slug in locations)) {
-            locations[slug] = {};
-        }
-
-        // find the field that corresponds to the per diem for this particular jurisidiction
-        let moneyTotal = [
-            'First 60 Days US$',
-            'Per Diem',
-            'Total residual',
-            'GRAND TOTAL (taxes included)',
-            'Amount (Euros)'
-        ]
-            .map(key => row[key])
-            .find(value => value);
-
-        // if we have a rate and it's the first season (the U.S. government pays by seasons)
-        // then we'll try to process this per diem
-        if (
-            typeof moneyTotal !== 'undefined' &&
-            moneyTotal !== '*' &&
-            (!row['Season Code'] || row['Season Code'] === 'S1')
-        ) {
-            // convert the rate to a float
-            moneyTotal = parseFloat(moneyTotal.replace(',', ''));
-
-            // if we're in the UK, add the room rate after possible conversion
-            if ('Room rate' in row) {
-                let roomRate = row['Room rate'];
-                let roomRateCurrency = roomRate.match(/\(*[A-Z]{3}\)*/);
-                roomRate = parseFloat(row['Room rate'].replace(/[^0-9.]*/g,''));
-
-                if (roomRateCurrency) {
-                    roomRate = money.convert(roomRate, {
-                        from: roomRateCurrency[0],
-                        to: 'USD'
-                    });
-                }
-
-                moneyTotal += roomRate;
-            }
-
-            // if the column header says this should be in Euros, respect that
-            if (row['Amount (Euros)']) {
-                row.Currency = 'EUR';
-            }
-
-            if (row.Currency) {
-                // find the currency code
-                let from = row.Currency.match(/\(*[A-Z]{3}\)*/);
-
-                // but if the column header says this should be in dollars, respect that
-                if (row['First 60 Days US$']) {
-                    from = null;
-                }
-
-                // if we found a three letter currency code...
-                if (from) {
-                    // no parens allowed
-                    from = from[0].replace('(', '').replace(')', '');
-
-                    // and if the currency code is in our JSON of rates
-                    if (from in currency.rates) {
-                        // convert it to USD
-                        moneyTotal = money.convert(moneyTotal, {
-                            from,
-                            to: 'USD'
-                        });
-                    } else {
-                        // otherwise, throw an error
-                        console.error(from + ' not in rates');
-                    }
-                }
-            }
-
-            // store the rate in its corresponding map location
-            locations[slug][file.replace('.csv', '')] = moneyTotal;
-
-            // include US meals & incidentals as a separate column for comparison to Canada
-            if ('Meals & Incidentals' in row) {
-                locations[slug]['US meals & incidentals'] = parseFloat(row['Meals & Incidentals'].replace(/[^0-9.]*/g,''));
-            }
-
-        }
-    });
-});
+// read all the files in /cleaned/ in one by one
+// each one corresponds to a jurisdiction like Canada, etc. that sets per diems
+files.forEach(processFile.bind(this, locations));
 
 // flatten the two layer map structure into an array of objects
 let locationRows = [];
@@ -153,4 +162,14 @@ Object.keys(locations).forEach(key => {
 });
 
 // output the result in CSV format
-console.log(dsv.csvFormat(locationRows));
+console.log(
+    dsv.csvFormat(locationRows, [
+        'slug',
+        'EU',
+        'UN',
+        'US',
+        'US meals & incidentals',
+        'Canada',
+        'UK'
+    ])
+);
